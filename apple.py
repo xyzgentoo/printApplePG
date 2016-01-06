e97f46c0cd40a79cee7fb28b48305a9100a255de
李鸿 <lihong@meitu.com>
2016-01-06 11:40:13 +0800

    [调整变量名，调整打印逻辑，遇到revision history就停止]

--- apple.py
+++ apple.py
@@ -10,57 +10,56 @@ from bs4 import BeautifulSoup
 import codecs
 import os
 
-# Quartz 2D Programming Guide
-# targetURL = 'https://developer.apple.com/library/ios/documentation/GraphicsImaging/Conceptual/drawingwithquartz2d/Introduction/Introduction.html#//apple_ref/doc/uid/TP40007533-SW1'
-# targetURL = 'https://developer.apple.com/library/ios/documentation/GraphicsImaging/Conceptual/drawingwithquartz2d/dq_overview/dq_overview.html#//apple_ref/doc/uid/TP30001066-CH202-TPXREF101'
-# targetURL = 'https://developer.apple.com/library/ios/documentation/GraphicsImaging/Conceptual/drawingwithquartz2d/dq_context/dq_context.html#//apple_ref/doc/uid/TP30001066-CH203-TPXREF101'
-# targetURL = 'https://developer.apple.com/library/ios/documentation/GraphicsImaging/Conceptual/drawingwithquartz2d/dq_paths/dq_paths.html#//apple_ref/doc/uid/TP30001066-CH211-TPXREF101'
 
-def outputPDF(fileNamePrefix, sourceURL):
+# 将Apple Programming Guide中的内容,生成print friendly pdfs
+def output_pdf(filename_prefix, source_url):
     # for Apple doc url, remove the part after #
-    targetURL = getTargetURL(sourceURL)
-    print targetURL
+    target_url = get_target_url(source_url)
+    print target_url
 
     # load url into html text strings
-    html = requests.get(targetURL)
+    html = requests.get(target_url)
 
     # 替换apple css为我自己修改后的css,为了输出print friendly样式的页面
     # <link rel="stylesheet" type="text/css" href="../../../../../Resources/1163/CSS/screen.css">
-    updatedHtml = re.sub(r'(<link rel=\"stylesheet\" type=\"text/css\" href=\")(.+screen.css)(\">)', r'\1https://cdn.rawgit.com/xyzgentoo/printApplePG/master/print.css\3', html.text)
+    updated_html_text = re.sub(r'(<link rel=\"stylesheet\" type=\"text/css\" href=\")(.+screen.css)(\">)',
+                         r'\1https://cdn.rawgit.com/xyzgentoo/printApplePG/master/print.css\3', html.text)
 
-    updatedHtml = re.sub(r'(<link rel=\"stylesheet\" type=\"text/css\" href=\")(.+feedback.css)(\">)', r'\1https://cdn.rawgit.com/xyzgentoo/printApplePG/master/feedback1.css\3', updatedHtml)
+    updated_html_text = re.sub(r'(<link rel=\"stylesheet\" type=\"text/css\" href=\")(.+feedback.css)(\">)',
+                         r'\1https://cdn.rawgit.com/xyzgentoo/printApplePG/master/feedback1.css\3', updated_html_text)
 
     ## replace img src=".. with img src="https://developer.apple.com/library/ios/documentation/GraphicsImaging/Conceptual/drawingwithquartz2d
-    resPrefixURL = getPrefixURL(targetURL)
+    res_prefix_url = get_prefix_url(target_url)
 
     # 替换img的相对路径
-    updatedHtml = re.sub(r'(<img src=\"../)(\w+)', r'<img src="' + resPrefixURL + r'\2', updatedHtml)
+    updated_html_text = re.sub(r'(<img src=\"../)(\w+)', r'<img src="' + res_prefix_url + r'\2', updated_html_text)
 
     # 注意 那几个js最好不要替换,要不里面也会报出找不到content的问题,pdfkit执行的时候就会报错了
 
-    items = targetURL.split('/')
-    outputPDFName = fileNamePrefix + '_' + items[len(items) - 1] + '.pdf'
+    items = target_url.split('/')
+    output_pdf_name = filename_prefix + '_' + items[len(items) - 1] + '.pdf'
 
     # use pdfkit to generate the pdf - 这里修改encoding是为了避免显示奇怪的字符
     # TODO python encoding这块还不是特别懂...
-    updatedHtml = updatedHtml.encode('latin-1')
-    # print updatedHtml
+    updated_html_text = updated_html_text.encode('latin-1')
+    # print updated_html_text
 
     # 这样用html文件中转一下,可以正确处理encoding的问题,如果使用pdfkit.from_string()也不能输出updatedHtml,估计是pdfkit内部错误
-    tempHtmlName = "./temp.html"
-    with codecs.open(tempHtmlName, "w") as f:
-        f.write(updatedHtml)
+    temp_html_file_name = "./temp.html"
+    with codecs.open(temp_html_file_name, "w") as f:
+        f.write(updated_html_text)
 
     # 从html文件生成pdf
-    pdfkit.from_file(tempHtmlName, outputPDFName)
+    pdfkit.from_file(temp_html_file_name, output_pdf_name)
 
     # clean up temp file
-    os.remove(tempHtmlName)
+    os.remove(temp_html_file_name)
     pass
 
+
 # get prefix URL to replace ../ and get absolute URL
-def getPrefixURL(targetURL):
-    items = targetURL.split('/')
+def get_prefix_url(target_url):
+    items = target_url.split('/')
     output = StringIO.StringIO()
 
     # 对应一个.. 然后最后一个不是path的一部分 于是-2 - 只把第一个../替换成URL中的部分,其他的相对路径在这个基础上就找到了
@@ -70,30 +69,32 @@ def getPrefixURL(targetURL):
     ret = output.getvalue()
     return ret
 
+
 # strip the # part in URL
-def getTargetURL(sourceURL):
-    return sourceURL.split('#')[0]
+def get_target_url(source_url):
+    return source_url.split('#')[0]
+
 
 # Starting of this program
 
 # REQUIRED
-filenamePrefix = 'PG_Quartz2D_'
+filename_prefix = 'PG_Predicate_'
 
 # REQUIRED - 这里记录去掉域名的部分,方便和后面的统一
-origURL = '/library/ios/documentation/GraphicsImaging/Conceptual/drawingwithquartz2d/Introduction/Introduction.html'
+orig_url = '/library/ios/documentation/Cocoa/Conceptual/Predicates/AdditionalChapters/Introduction.html'
 
 # REQUIRED - DO NOT CHANGE FOR APPLE Programming Guides
-urlPrefix = 'https://developer.apple.com'
+domain_prefix = 'https://developer.apple.com'
 
 i = 0
 
 # 记录已经处理过的URLs
-handled = [origURL]
+handled = [orig_url]
 
 # output introduction page as PDF first
-finalOrigURL = urlPrefix + origURL
-print 'orig: ' + finalOrigURL
-outputPDF(filenamePrefix + str(i), finalOrigURL)
+final_orig_url = domain_prefix + orig_url
+print 'orig: ' + final_orig_url
+output_pdf(filename_prefix + str(i), final_orig_url)
 i += 1
 
 # output other pages as PDFs
@@ -108,22 +109,31 @@ print 'start splashing...'
 # * Run = eval "$(docker-machine env default)" = in terminal to init docker environment
 # * Run = docker run -p 5023:5023 -p 8050:8050 -p 8051:8051 scrapinghub/splash & = in terminal to start splash
 # * Run = docker-machine ip default = in terminal to get the IP of docker machine
-splashURL = 'http://192.168.99.100:8050/render.html?timeout=60&url=' + finalOrigURL
-htmlObj = requests.get(splashURL)
+splashURL = 'http://192.168.99.100:8050/render.html?timeout=60&url=' + final_orig_url
+html_obj = requests.get(splashURL)
 
-soup = BeautifulSoup(htmlObj.text, 'html.parser')
+soup = BeautifulSoup(html_obj.text, 'html.parser')
 
 sections = soup.find_all('span', {'class': 'sectionName'})
+print "len(sections): " + str(len(sections))
+
 # 直接忽略
 for section in sections:
-    link = getTargetURL(section.a['href'])
+    link = get_target_url(section.a['href'])
 
-    # 这里得到的link都是不包含域名的版本
-    if link not in handled:
-        finalURL = urlPrefix + link
-        print 'new: ' + finalURL
-        outputPDF(filenamePrefix + str(i), finalURL)
-        i += 1
-        handled.append(link)
+    # 这里要过滤一下API Reference的链接
+    if None == re.match(r'(\/.+Reference\/.*\/index.html)', link):
+        # 这里得到的link都是不包含域名的版本
+        if link not in handled:
+            final_url = domain_prefix + link
+            print 'new: ' + final_url
+            output_pdf(filename_prefix + str(i), final_url)
+            i += 1
+            handled.append(link)
 
-print "COOOOOL..."
+            # 如果处理完版本更新的历史,就停止了,因为有些guides后面会跟着其他的链接,不支持的格式
+            if link.endswith('RevisionHistory.html'):
+                print '--- TO END ---'
+                break
+
+print '*' * 50
